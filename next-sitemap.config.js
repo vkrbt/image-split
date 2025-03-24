12f717fbdf7d660ffe43fa54ff4b3afc2d42e46d
Vlad Korbut <v.korbut8@gmail.com>
2025-03-24 16:34:19 +0300

    chore: update sitemap.xml and next-sitemap config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -14,4 +14,18 @@ module.exports = {
   priority: 1.0,
   sitemapSize: 7000,
   exclude: ['/404', '/500'],
+  transform: async (config, path) => {
+    // Set different priorities for different pages
+    const priorities = {
+      '/': 1.0,
+      '/instagram-guide': 0.8,
+    };
+
+    return {
+      loc: path,
+      changefreq: config.changefreq,
+      priority: priorities[path] || 0.6,
+      lastmod: new Date().toISOString(),
+    };
+  },
 } 
